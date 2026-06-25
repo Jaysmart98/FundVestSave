c4b9d1ffe0189751707a47b684d42e978d6cde4c
Jaysmart <ogunbunmijoshua60@gmail.com>
2026-06-25 10:49:40 +0100

    Fix import paths in App component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home.jsx';
-import About from './Pages/About/About.jsx'
-import Investment from './Pages/Investment/Investment.jsx'
-import Recovery from './Pages/ScamRecovery/Recovery.jsx'
-import Loan from './Pages/Loans/Loan.jsx';
-import Career from './Pages/Career/Career.jsx';
-import FAQ from './Pages/FAQ/FAQ.jsx';
-import Legal from './Pages/Legal/Legal.jsx';
-import Contact from './Pages/Contact/Contact.jsx';
+import About from '../src/Pages/About/About.jsx'
+import Investment from '../src/Pages/Investment/Investment.jsx'
+import Recovery from '../src/Pages/ScamRecovery/Recovery.jsx'
+import Loan from '../src/Pages/Loans/Loan.jsx';
+import Career from '../src/Pages/Career/Career.jsx';
+import FAQ from '../src/Pages/FAQ/FAQ.jsx';
+import Legal from '../src/Pages/Legal/Legal.jsx';
+import Contact from '../src/Pages/Contact/Contact.jsx';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -17,15 +17,15 @@ function App() {
     <>
       <ToastContainer position="top-right" autoClose={3000} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/investment" element={<Investment />} />
-        <Route path="/recovery" element={<Recovery />} />
-        <Route path="/loan" element={<Loan />} />
-        <Route path="/career" element={<Career />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/legal" element={<Legal />} />
-        <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/investment" element={<Investment />} />
+          <Route path="/recovery" element={<Recovery />} />
+          <Route path="/loan" element={<Loan />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
